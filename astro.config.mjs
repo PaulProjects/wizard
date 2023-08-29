d60eca71f2a3de47ae52550f364cbf57c23df0d9
Paul <77199889+PaulProjects@users.noreply.github.com>
2023-08-29 12:27:42 +0200

    sitemap
    +seo -jquery on index +presets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import prefetch from "@astrojs/prefetch";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch()],
-  site: 'https://wizard.github.io',
-  base: '/',
+  integrations: [tailwind(), prefetch(), sitemap()],
+  site: 'https://wizzard.site',
+  base: '/'
 });
