35e1d95ea4dc1fab7d62f70ce33b74b366d4095c
Paul <77199889+PaulProjects@users.noreply.github.com>
2024-02-06 15:53:01 +0100

    update astro and add analytics page + qol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
