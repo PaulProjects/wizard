2d5d0c3bd8b24a4800c56650b91290f83e1f3f43
Paul <77199889+PaulProjects@users.noreply.github.com>
2024-03-03 11:33:32 +0100

    Refactor Score.js, Removed Clicky, Datenschutz update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import matomo from 'astro-matomo';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch(), sitemap()],
+  integrations: [tailwind(), prefetch(), sitemap(), matomo({
+    enabled: true,
+    host: "https://paulbertram.de/wa/",
+    trackerUrl: "js/", // defaults to matomo.php
+    srcUrl: "js/", // defaults to matomo.js
+    siteId: 2,
+    heartBeatTimer: 5,
+    disableCookies: true,
+  }),],
   site: 'https://wizzard.site',
   base: '/'
 });
