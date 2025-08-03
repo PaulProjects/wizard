5a07c1f8fd35cab332023a304a2cf643ba0c2ed5
Paul <bertram.paul@icloud.com>
2025-08-03 11:48:45 +0200

    Refactor: Version upgrade, Analytics, Privacy and Impressum Location

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch(),
+  integrations: [tailwind(), 
     sitemap({
 			filter: (page) =>
 				page == "https://wizard.paulbertram.de/"
 		}),
+    icon({
+      include: {
+        mdi: ["*"], // Include all Material Design Icons
+        mi: ["*"]   // Include all Minimal Icons
+      }
+    }),
   ],
   site: 'https://wizard.paulbertram.de',
 });
